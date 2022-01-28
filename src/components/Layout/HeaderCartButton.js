467f09cad79e274d3036a2951cd339c30c8c6cfb
Jakub Wojcik <j.wojcik1994@gmail.com>
2022-01-28 12:57:20 +0100

    Add useEffect to generate a bump animation on the cart button in header when product was added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = ({ showCart }) => {
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberofCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const numberofCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnStyles = `${styles.button} ${btnIsAnimated ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsAnimated(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsAnimated(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={showCart}>
+    <button className={btnStyles} onClick={showCart}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
